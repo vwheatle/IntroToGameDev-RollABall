01fbf4306b4c097a7ba63e4922304b3931a9c608
vwheatle <vwheatle@kent.edu>
2022-10-08 00:22:09 -0400

    pickup visual sine wave thing

--- Assets/Scripts/Pickup.cs
+++ Assets/Scripts/Pickup.cs
@@ -3,11 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Pickup : MonoBehaviour {
+	private Vector2 xzPos;
+	
 	void Start() {
-		
+		xzPos = new Vector2(transform.position.x, transform.position.z);
+		// myChild.rotation = Random.rotationUniform;
 	}
 	
 	void Update() {
+		float funny = Time.realtimeSinceStartup + (xzPos.x + xzPos.y) / 8f;
+		float yOffset = Mathf.Abs(Mathf.Cos(Mathf.PI * 2 * funny)) / 4f;
+		
+		transform.localPosition = Vector3.up * yOffset;
 		transform.Rotate(new Vector3(15, 30, 0) * Time.deltaTime);
 	}
 }
